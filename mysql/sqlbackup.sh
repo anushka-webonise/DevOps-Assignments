160d54c030f00c6c8232f1340e1bdd5087cc73a0
anushka-webonise <90310514+anushka-webonise@users.noreply.github.com>
2021-09-16 15:17:52 +0530

    Update sqlbackup.sh

--- mysql/sqlbackup.sh
+++ mysql/sqlbackup.sh
@@ -1,9 +1,9 @@
 DBNAME=employees
 DATE=`date +"%Y%m%d"`
 SQLFILE=$DBNAME-${DATE}.sql
-#SQLFILE=annewroot2.sql
+
 echo "Creating backup"
-sudo mysqldump --opt --user=root --password $DBNAME > $SQLFILE
+sudo mysqldump --opt --user=root $DBNAME > $SQLFILE
 gzip $SQLFILE
 echo "Done"
 
